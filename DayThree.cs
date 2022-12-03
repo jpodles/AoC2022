976c289d5fb99989a12d0f325e0019458bcf557d
Jakub Podleś <jakubpodles98@gmail.com>
2022-12-03 10:59:55 +0100

    Day Three

---
+++ DayThree.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AoC2022
+{
+    internal class DayThree : IAocResults
+    {
+        public static async Task<ITuple> PartOneAndTwo() => (await PartOne(), await PartTwo());
+
+        async static Task<int> PartOne()
+        {
+            var result = 0;
+            await foreach (var line in File.ReadLinesAsync("./Data/DayThree.txt"))
+            {
+                var firstRucksack = line.Take(line.Length / 2).ToArray();
+                var secondRucksack = line.TakeLast(line.Length / 2).ToArray();
+
+                result += GetValue(firstRucksack.Intersect(secondRucksack).Select(x => x).First());
+            }
+            return result;
+        }
+
+        async static Task<int> PartTwo()
+        {
+            var groupCount = 0;
+            var listOfGroups = new List<List<List<char>>>();
+            var groupList = new List<List<char>>();
+            await foreach (var line in File.ReadLinesAsync("./Data/DayThree.txt"))
+            {
+                groupList.Add(line.ToList());
+                groupCount++;
+
+                if (groupCount == 3)
+                {
+                    listOfGroups.Add(groupList);
+                    groupList = new List<List<char>>();
+                    groupCount = 0;
+                }
+            }
+
+            var results = new List<HashSet<char>>();
+            foreach (var list in listOfGroups)
+            {
+                var hashSet = new HashSet<char>(list.First());
+                foreach (var items in list.Skip(1))
+                {
+                    hashSet.IntersectWith(items);
+                }
+                results.Add(hashSet);
+            }
+
+            return results.Select(x => GetValue(x.First())).Sum();
+        }
+
+        static int GetValue(char item) => char.IsLower(item) ? item - 96 : item - 38;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿
-
-using AoC2022;
-
-var dayOne = await IAocResults.RunAndGetResults(DayOne.PartOneAndTwo);
-var dayTwo = await IAocResults.RunAndGetResults(DayTwo.PartOneAndTwo);
-
+﻿using AoC2022;
 
 var results = new List<Result>()
     .Concat(await IAocResults.RunAndGetResults(DayOne.PartOneAndTwo))
-    .Concat(await IAocResults.RunAndGetResults(DayTwo.PartOneAndTwo));
+    .Concat(await IAocResults.RunAndGetResults(DayTwo.PartOneAndTwo))
+    .Concat(await IAocResults.RunAndGetResults(DayThree.PartOneAndTwo));
 
+
+//TODO: Spectre.Console table
+
+var xd = await IAocResults.RunAndGetResults(DayThree.PartOneAndTwo);
+Console.ReadLine();
