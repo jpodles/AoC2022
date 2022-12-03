38267ef3735c687bf626cff7789d6bd021b3faca
Jakub Podleś <jakubpodles98@gmail.com>
2022-12-03 16:09:44 +0100

    day 3 functional approach

--- DayThree.cs
+++ DayThree.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
 
 namespace AoC2022
 {
@@ -6,52 +7,18 @@ namespace AoC2022
     {
         public static async Task<ITuple> PartOneAndTwo() => (await PartOne(), await PartTwo());
 
-        async static Task<int> PartOne()
-        {
-            var result = 0;
-            await foreach (var line in File.ReadLinesAsync("./Data/DayThree.txt"))
-            {
-                var firstRucksack = line.Take(line.Length / 2).ToArray();
-                var secondRucksack = line.TakeLast(line.Length / 2).ToArray();
+        async static Task<int> PartOne() => (await File.ReadAllLinesAsync("./Data/DayThree.txt"))
+            .Select(line => (First: line.Take(line.Length / 2), Second: line.TakeLast(line.Length / 2)))
+            .Select(group => group.First.Intersect(group.Second).Single())
+            .Select(GetPriority)
+            .Sum();
 
-                result += GetValue(firstRucksack.Intersect(secondRucksack).Select(x => x).First());
-            }
-            return result;
-        }
+        async static Task<int> PartTwo() => (await File.ReadAllLinesAsync("./Data/DayThree.txt"))
+                .Chunk(3)
+                .Select(group => group[0].Intersect(group[1]).Intersect(group[2]).Single())
+                .Select(GetPriority)
+                .Sum();
 
-        async static Task<int> PartTwo()
-        {
-            var groupCount = 0;
-            var listOfGroups = new List<List<List<char>>>();
-            var groupList = new List<List<char>>();
-            await foreach (var line in File.ReadLinesAsync("./Data/DayThree.txt"))
-            {
-                groupList.Add(line.ToList());
-                groupCount++;
-
-                if (groupCount == 3)
-                {
-                    listOfGroups.Add(groupList);
-                    groupList = new List<List<char>>();
-                    groupCount = 0;
-                }
-            }
-
-            var results = new List<HashSet<char>>();
-            listOfGroups.ForEach(list =>
-            {
-                var intersection = list
-                   .Skip(1)
-                   .Aggregate(
-                       new HashSet<char>(list.First()),
-                       (a, b) => { a.IntersectWith(b); return a; }
-                   );
-
-                results.Add(intersection);
-            });
-            return results.Select(x => GetValue(x.First())).Sum();
-        }
-
-        static int GetValue(char item) => char.IsLower(item) ? item - 96 : item - 38;
+        static int GetPriority(char item) => char.IsLower(item) ? item - 96 : item - 38;
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,5 +5,4 @@ var results = new List<Result>()
     .Concat(await IAocResults.RunAndGetResults(DayTwo.PartOneAndTwo))
     .Concat(await IAocResults.RunAndGetResults(DayThree.PartOneAndTwo));
 
-
 //TODO: Spectre.Console table
